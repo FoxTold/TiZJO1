e99447c3d6b8ce980113ee771c1fea609d21396b
FoxTold <rybawoda2@gmail.com>
2023-03-13 16:15:28 +0100

    added template of lab 2

--- lab3/main.py
+++ lab3/main.py
@@ -3,18 +3,19 @@
 import sys
 
 def my_printf(format_string,param):
-    #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
-                shouldDo=False
-            else:
-                print(format_string[idx],end="")
-        else:
-            shouldDo=True
-    print("")
+    if format_string == "" or param == "":
+        print("")
+        return
+    znalazlem = re.search("#(\.\d+)?k", format_string)
+    if not znalazlem:
+        print(format_string)
+        return
+    zamiana = format_string[znalazlem.start():znalazlem.end()]
+    length = len(param)
+    if znalazlem.group(1):
+        length = int(znalazlem.group(1)[1:])
+    do_wypisania = format_string.replace(zamiana, param.swapcase()[:min(len(param),length)])
+    print(do_wypisania)
 
 data=sys.stdin.readlines()
 
