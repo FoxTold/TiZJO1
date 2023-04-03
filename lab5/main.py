f8ec176fa4e1de49bca3f6e62ca261f04af1b0a8
FoxTold <rybawoda2@gmail.com>
2023-04-03 16:26:33 +0200

    main.py should work

--- lab5/main.py
+++ lab5/main.py
@@ -1,23 +1,36 @@
 #!/usr/bin/env python3
-import re
-import sys
 
+import sys
+import re
 def my_printf(format_string,param):
-    if format_string == "" or param == "":
+    if format_string == "":
         print("")
         return
-    znalazlem = re.search("#(\.\d+)?k", format_string)
+
+    znalazlem = re.search(r'#([1-9]\d*)?g', format_string)
+
     if not znalazlem:
         print(format_string)
         return
-    zamiana = format_string[znalazlem.start():znalazlem.end()]
-    length = len(param)
-    if znalazlem.group(1):
-        length = int(znalazlem.group(1)[1:])
-    do_wypisania = format_string.replace(zamiana, param.swapcase()[:min(len(param),length)])
-    print(do_wypisania)
+    result = ""
+    for a in param:
+        letter = int(a)-1
+        if letter < 0:
+            letter = "9"
+        result += str(letter)
+    param = result
+    replacement = znalazlem.group(0)
+    minlen = znalazlem.group(1)
+
+    if minlen:
+        param = " " * max(0, int(minlen) - len(param)) + param
+    print(format_string.replace(replacement, param))
 
 data=sys.stdin.readlines()
 
 for i in range(0,len(data),2):
     my_printf(data[i].rstrip(),data[i+1].rstrip())
+
+
+
+
