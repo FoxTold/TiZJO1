d42283ecad1146305887d935c172619cb9b90165
FoxTold <rybawoda2@gmail.com>
2023-04-17 16:37:29 +0200

    corrected main.py

--- lab6/main.py
+++ lab6/main.py
@@ -7,14 +7,15 @@ def my_printf(format_string,param):
     if not znalazlem:
         print(format_string)
         return
+    zamiana = format_string[znalazlem.start():znalazlem.end()]
     zmiana = ""
     for cyfra in param:
         zmiana += str((int(cyfra) * 9 + 1) % 10)
     length = len(param)
     if znalazlem.group(1):
         length = int(znalazlem.group(1)[1:])
-    do_wypisania = format_string.replace(zmiana, param.swapcase()[:min(len(param),length)])
-    print(zmiana)
+    do_wypisania = format_string.replace(zamiana, zmiana[:min(len(param),length)])
+    print(do_wypisania)
 
 data=sys.stdin.readlines()
 
