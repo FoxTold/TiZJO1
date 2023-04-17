3fbfa745763f62efde79c32e846a6619df1bdc38
FoxTold <rybawoda2@gmail.com>
2023-04-17 16:53:23 +0200

    new tests added, implementation of main changed

--- lab6/main.py
+++ lab6/main.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3
-import re
-import sys
 
-def my_printf(format_string,param):
-    znalazlem = re.search("#(\.\d+)?g", format_string)
-    if not znalazlem:
+import sys
+import re
+
+
+def zamiana_litery(old_cyfra):
+    return (old_cyfra * 9 + 1) % 10
+def my_printf(format_string, param):
+    match = re.search("#\.(\d+)g", format_string)
+    if not match:
         print(format_string)
         return
-    zamiana = format_string[znalazlem.start():znalazlem.end()]
-    zmiana = ""
-    for cyfra in param:
-        zmiana += str((int(cyfra) * 9 + 1) % 10)
-    length = len(param)
-    if znalazlem.group(1):
-        length = int(znalazlem.group(1)[1:])
-    do_wypisania = format_string.replace(zamiana, zmiana[:min(len(param),length)])
-    print(do_wypisania)
+    replacement , number = match.group(0) , int(match.group(1))
+    param,absparam,zeros = int(param),abs(int(param)), max(0, number - len(str(abs(int(param)))))
+    replace_with = (zeros * "0") + str(absparam)
+    replace_with = ('-' if param < 0 else '') + ''.join([str(zamiana_litery(int(letter))) for letter in replace_with])
+    print(format_string.replace(replacement, replace_with))
 
-data=sys.stdin.readlines()
-
-for i in range(0,len(data),2):
-    my_printf(data[i].rstrip(),data[i+1].rstrip())
+data = sys.stdin.readlines()
+for i in range(0, len(data), 2):
+    my_printf(data[i].rstrip(), data[i + 1].rstrip())
