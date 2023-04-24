d45702021df27686a8c55831f73f33ecd59db5cb
FoxTold <rybawoda2@gmail.com>
2023-04-24 16:11:13 +0200

    initial commit

---
+++ lab7/test.py
@@ -0,0 +1,18 @@
+import sys
+import re
+
+def my_printf(format_string,param):
+    if format_string == "":
+        print("")
+        return
+    znalazlem = re.search("#(\.\d+)?k", format_string)
+    if not znalazlem:
+        print(format_string)
+        return
+    zamiana = format_string[znalazlem.start():znalazlem.end()]
+    length = len(param)
+    if znalazlem.group(1):
+        length = int(znalazlem.group(1)[1:])
+    print(format_string.replace(zamiana, param.swapcase()[:min(len(param),length)]))
+
+my_printf("## kk# []][[[] #-------k #--k #-2.k #.-2k,,...,,.","asd")
