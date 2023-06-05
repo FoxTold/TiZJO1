c478d84a439be55494b9d1cc1bdb5f1a8a36a746
FoxTold <rybawoda2@gmail.com>
2023-06-05 17:55:07 +0200

    changes changes

--- lab11/main.py
+++ lab11/main.py
@@ -2,21 +2,29 @@
 
 import sys
 
-def new_number(old_number):
-    new_number = int((old_number * 2) / len(str(old_number)))
-    if new_number % 2 != 0:
-        return str(hex(new_number)).replace('0x', '')
-    return str(new_number)
+
+def changeee(param):
+    binary = bin(param).replace('0b', '')
+    changed_binary = ''
+    for i, num in enumerate(binary[::-1]):
+        if num == '0':
+            changed_binary += '0'
+        else:
+            changed_binary += 'abcdefghij'[(i)%10]
+    changed_binary = changed_binary[::-1]
+    return changed_binary
+
+
 def my_printf(format_string, param):
-    if '#a' not in format_string:
+    replace = '#b'
+    if replace not in format_string:
         print(format_string)
         return
-    replace = '#a'
     param = int(param)
-    replace_with = new_number(param)
+    replace_with = changeee(param)
     print(format_string.replace(replace, replace_with))
 
-
 data = sys.stdin.readlines()
+
 for i in range(0, len(data), 2):
     my_printf(data[i].rstrip(), data[ i + 1].rstrip())
