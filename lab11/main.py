29efc5627622d6d0c2689d397b3064fde8d86909
FoxTold <rybawoda2@gmail.com>
2023-06-05 18:10:20 +0200

    main changes with new tests

--- lab11/main.py
+++ lab11/main.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
-
 import sys
-
-
-def changeee(param):
-    binary = bin(param).replace('0b', '')
+def changesssss(param):
+    binary = bin(param).replace('-0b', '').replace('0b', '')
     changed_binary = ''
     for i, num in enumerate(binary[::-1]):
         if num == '0':
@@ -13,18 +10,17 @@ def changeee(param):
             changed_binary += 'abcdefghij'[(i)%10]
     changed_binary = changed_binary[::-1]
     return changed_binary
-
-
 def my_printf(format_string, param):
     replace = '#b'
     if replace not in format_string:
         print(format_string)
         return
-    param = int(param)
-    replace_with = changeee(param)
+    try:
+        param = int(param)
+    except Exception:
+        param = 0
+    replace_with = changesssss(param)
     print(format_string.replace(replace, replace_with))
-
 data = sys.stdin.readlines()
-
 for i in range(0, len(data), 2):
     my_printf(data[i].rstrip(), data[ i + 1].rstrip())
